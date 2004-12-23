c6c684d22ff97cbc4814da9ceadf9e9b206656ab
Jan Dockx <jan_dockx@peopleware.be>
2004-12-23 16:17:20 +0000

    Ready to deploy?
    git-svn-id: file:///repositories/PPWCode/trunk/ppw-bean@133 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

---
+++ src/java/be/peopleware/bean_I/persistent/IdNotFoundException.java
@@ -0,0 +1,87 @@
+package be.peopleware.bean_I.persistent;
+
+
+/**
+ * <p>This exception signals failure to locate a object with the given ID.</p>
+ *
+ * @invar     getId() != null;
+ *
+ * @author    Jan Dockx
+ * @author    David Van Keer
+ * @author    PeopleWare n.v.
+ */
+public class IdNotFoundException extends IdException {
+
+  /*<section name="Meta Information">*/
+  //------------------------------------------------------------------
+
+  /** {@value} */
+  public static final String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_DATE = "$Date$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
+  /** {@value} */
+  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
+
+  /*</section>*/
+
+
+
+
+  /*<construction>*/
+  //------------------------------------------------------------------
+
+  /**
+   * @param     id
+   *            The id for which we did not find a Object.
+   * @param     message
+   *            The message that describes the exceptional circumstance.
+   * @param     cause
+   *            The exception that occured, causing this exception to be
+   *            thrown, if that is the case.
+   * @param     beanClass
+   *            The bean class which is involved in this Exception.
+   *
+   * @pre       (message != null) ==> ! message.equals("");
+   * @pre       id != null;
+   * @pre       beanClass != null;
+   * @post      (message != null) ==> new.getMessage().equals(message);
+   * @post      (message == null) ==> new.getMessage() == null;
+   * @post      new.getCause() == cause;
+   * @post      new.getLocalizedMessageResourceBundleLoadStrategy().getClass()
+   *                == DefaultResourceBundleLoadStrategy.class;
+   * @post      new.getBeanClassName() == beanClassName;
+   * @post      new.getId().equals(id);
+   */
+  public IdNotFoundException(final Long id,
+                             final String message,
+                             final Throwable cause,
+                             final Class beanClass) {
+    super(message, cause, beanClass);
+    assert id != null;
+    $id = id;
+  }
+
+  /*</construction;>*/
+
+
+
+  /*<property name="id">*/
+  //------------------------------------------------------------------
+
+  /**
+   * @basic
+   */
+  public final Long getId() {
+    return $id;
+  }
+
+  /**
+   * @invar     $id != null;
+   */
+  private Long $id;
+
+  /*</property>*/
+
+}
