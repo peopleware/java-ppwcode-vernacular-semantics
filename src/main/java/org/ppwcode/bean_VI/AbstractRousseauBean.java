108b990785e280f54b3e655d29164a43ff6a4efa
Jan Dockx <jan_dockx@peopleware.be>
2008-06-16 10:36:58 +0000

    quick "make it compile"
    git-svn-id: file:///repositories/PPWCode/java/bean/trunk@1177 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/bean_VI/AbstractRousseauBean.java
+++ src/main/java/org/ppwcode/bean_VI/AbstractRousseauBean.java
@@ -1,10 +1,23 @@
 /*<license>
-  Copyright 2004, PeopleWare n.v.
-  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
-  TO SELECTED PARTIES.
+Copyright 2004 - $Date$ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 </license>*/
 
 package org.ppwcode.bean_VI;
+
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
 
 import java.util.Collection;
 import java.util.Iterator;
@@ -13,6 +26,9 @@ import java.util.Set;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
 
 
 /**
@@ -21,22 +37,11 @@ import org.apache.commons.logging.LogFactory;
  * @author    nsmeets
  * @author    PeopleWare n.v.
  */
+@Copyright("2004 - $Date$, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 public abstract class AbstractRousseauBean implements RousseauBean {
-
-  /*<section name="Meta Information">*/
-  //------------------------------------------------------------------
-
-  /** {@value} */
-  public static final String CVS_REVISION = "$Revision:1123 $"; //$NON-NLS-1$
-  /** {@value} */
-  public static final String CVS_DATE = "$Date:2008-06-12 22:26:03 +0200 (Thu, 12 Jun 2008) $"; //$NON-NLS-1$
-  /** {@value} */
-  public static final String CVS_STATE = "$State$"; //$NON-NLS-1$
-  /** {@value} */
-  public static final String CVS_TAG = "$Name$"; //$NON-NLS-1$
-
-  /*</section>*/
-
 
   private static final Log LOG = LogFactory.getLog(AbstractRousseauBean.class);
 
