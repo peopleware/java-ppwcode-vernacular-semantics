74a0e7e9135719b959fc215f8d2e92a17820a00e
Jan Dockx <jan_dockx@peopleware.be>
2008-06-19 22:34:39 +0000

    Cleanup of Abstract/RousseauBean. Missing unit test.
    git-svn-id: file:///repositories/PPWCode/java/bean/trunk@1193 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- src/main/java/org/ppwcode/bean_VI/AbstractRousseauBean.java
+++ src/main/java/org/ppwcode/bean_VI/AbstractRousseauBean.java
@@ -20,7 +20,6 @@ package org.ppwcode.bean_VI;
 import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
 
 import java.util.Collection;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.Set;
 
@@ -29,73 +28,100 @@ import org.apache.commons.logging.LogFactory;
 import org.ppwcode.metainfo_I.Copyright;
 import org.ppwcode.metainfo_I.License;
 import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.MethodContract;
+import org.toryt.annotations_I.Scope;
 
 
 /**
- * A partial implementation of the interface {@link RousseauBean}.
+ * <p>A partial implementation of the interface {@link RousseauBean}.</p>
+ * <p>Subclasses should take care to override {@link #getWildExceptions()} diligently,
+ *   to add validation concerning properties and civility invariants added in the subclass.</p>
  *
  * @author    nsmeets
+ * @author    Jan Dockx
  * @author    PeopleWare n.v.
+ *
+ * @mudo unit test
  */
 @Copyright("2004 - $Date$, PeopleWare n.v.")
 @License(APACHE_V2)
 @SvnInfo(revision = "$Revision$",
          date     = "$Date$")
-public abstract class AbstractRousseauBean implements RousseauBean {
+public abstract class AbstractRousseauBean extends AbstractSemanticBean implements RousseauBean {
 
   private static final Log LOG = LogFactory.getLog(AbstractRousseauBean.class);
 
   /**
-   * @see  RousseauBean
+   * {@inheritDoc}
    *
    * @protected
    * <p><em>The implementation in this class returns an emtpy
-   *   CompoundPropertyException
-   *   (<code>result.getElementExceptions().isEmpty();</code>).</em>
-   *   Subclasses should override this method.</p>
+   *   CompoundPropertyException.</em> Subclasses should override this method
+   *   with the following idiom:</p>
+   * <pre>
+   * ATOverride
+   * ATMethodContract(
+   *   post = {
+   *     ATExpression(&quot;'someProperty <var>condition</var> ?? result.contains(new <var>XXXPropertyException(...)</var>)&quot;),
+   *     ...
+   *     ATExpression(&quot;for (PropertyException pe : result.allElementExceptions) {&quot; +
+   *                  &quot;pe.like(new <var>XXXPropertyException(...)</var>) || &quot; +
+   *                  &quot;...&quot;)
+   *   }
+   * )
+   * public CompoundPropertyException getWildExceptions() {
+   *   CompoundPropertyException cpe = super.getWildExceptions();
+   *   if (getSomeProperty() <var>condition</var>) {
+   *     cpe.addElementException(new <var>XXXPropertyException(...)</var>));
+   *   }
+   *   ...
+   *   return cpe;
+   * }
+   * </pre>
    */
+  @MethodContract(
+    post = {
+      @Expression(scope = Scope.PROTECTED, value = "result.origin == this"),
+      @Expression(scope = Scope.PROTECTED, value = "result.propertyName == null"),
+      @Expression(scope = Scope.PROTECTED, value = "result.message == null"),
+      @Expression(scope = Scope.PROTECTED, value = "result.cause == null"),
+      @Expression(scope = Scope.PROTECTED, value = "result.elementExceptions.empty")
+    }
+  )
   public CompoundPropertyException getWildExceptions() {
     return new CompoundPropertyException(this, null, null, null);
   }
 
   /**
-   * @see  RousseauBean
+   * {@inheritDoc}
    */
   public final boolean isCivilized() {
     return getWildExceptions().isEmpty();
   }
 
   /**
-   * This method does nothing, but will throw the wild exceptions
-   * if this bean is not civilized.
+   * {@inheritDoc}
    *
-   * @see  RousseauBean
+   * This method logs progress on the debug level.
    */
   public final void checkCivility() throws CompoundPropertyException {
     CompoundPropertyException cpe = getWildExceptions();
     if (!cpe.isEmpty()) {
-      if (LOG.isDebugEnabled()) { // this if does only loggin
-        LOG.debug("the wild exceptions were not empty; we will throw the "
-            + "CompoundPropertyException"); //$NON-NLS-1$
-        LOG.debug("current RousseauBean is " + toString()); //$NON-NLS-1$
+      if (LOG.isDebugEnabled()) { // this if does only logging
+        LOG.debug("the wild exceptions were not empty; we will throw the CompoundPropertyException");
+        LOG.debug("current RousseauBean is " + toString());
         LOG.debug(cpe.getElementExceptions());
-        LOG.debug("ElementExceptions details:"); //$NON-NLS-1$
-        Iterator iter =  cpe.getElementExceptions().entrySet().iterator();
-        while (iter.hasNext()) {
-          Map.Entry entry = (Map.Entry)iter.next();
-          if (LOG.isDebugEnabled()) {
-            Object key = entry.getKey();
-            if (key == null) {
-              LOG.debug("null as key:");
-            }
-            else {
-              LOG.debug(key + ":");
-            }
+        LOG.debug("ElementExceptions details:");
+        for (Map.Entry<String, Set<PropertyException>> entry : cpe.getElementExceptions().entrySet()) {
+          Object key = entry.getKey();
+          if (key == null) {
+            LOG.debug("null as key:");
           }
-          Set propertyExceptions = (Set)entry.getValue();
-          Iterator exceptions = propertyExceptions.iterator();
-          while (exceptions.hasNext()) {
-            PropertyException pexc = (PropertyException)exceptions.next();
+          else {
+            LOG.debug(key + ":");
+          }
+          for (PropertyException pexc : entry.getValue()) {
             LOG.debug(pexc, pexc);
           }
         }
@@ -106,79 +132,32 @@ public abstract class AbstractRousseauBean implements RousseauBean {
   }
 
   /**
-   * @see  RousseauBean
+   * {@inheritDoc}
    *
    * @protected
-   *  The implementation of this method in this class does NOP,
-   *  as a convenience to most subtypes.
+   * The implementation of this method in this class does NOP,
+   * as a convenience to most subtypes.
    */
   public void normalize() {
     // NOP
   }
 
   /**
-   * This instance has the same values as the instance <code>rb</code>.
-   * This comparison does not take into account to-many associations,
-   * to avoid infinite loops. Comparison is independent of normalization,
-   * i.e., it should return the same result when the participants in the
-   * comparison are normalized or not. To accomplish that, this method
-   * may call {@link #normalize()} itself.
+   * Convenience method for generating the toString of to-many associations.
    *
-   * @see  RousseauBean
+   * @mudo move somewhere else
    */
-  public boolean hasSameValues(final RousseauBean rb) {
-    return (rb != null) && (getClass() == rb.getClass());
-  }
-
-  /**
-   * @see  RousseauBean
-   */
-  public final boolean equals(final Object other) {
-    return super.equals(other);
-  }
-
-  /**
-   * @see  RousseauBean
-   */
-  public final int hashCode() {
-    return super.hashCode();
-  }
-
-  /**
-   * Convenience method for generating the toString
-   * of to-many associations.
-   *
-   * @pre       c != null;
-   */
-  protected final String collectionString(final Collection c) {
+  @MethodContract(pre = @Expression("c != null"), post = @Expression("true"))
+  protected final static String collectionString(final Collection<?> c) {
     assert c != null;
-    String result = "{"; //$NON-NLS-1$
-    for (Iterator i = c.iterator(); i.hasNext();) {
-        Object item = i.next();
-        result = result + item.getClass().getName()
-                     + "@" + item.hashCode(); //$NON-NLS-1$
-        if (i.hasNext()) {
-          result = result + ", "; //$NON-NLS-1$
-        }
+    StringBuilder result = new StringBuilder("{");
+    for (Object item : c) {
+      result.append(item.getClass().getName());
+      result.append("@");
+      result.append(Integer.toHexString(item.hashCode()));
     }
-    result = result + "}"; //$NON-NLS-1$
-    return result;
-  }
-
-  /**
-   * Returns true when the two given objects are both null, or equal;
-   * returns false otherwise.
-   *
-   * @param   one
-   * @param   other
-   * @return  (one == null)
-   *            ? result == (other == null)
-   *            : result == (one.hasSameValues(other));
-   */
-  public static boolean hasSameValuesWithNull(final RousseauBean one, final RousseauBean other) {
-    return (one == null)
-            ? (other == null)
-            : one.hasSameValues(other);
+    result.append("}");
+    return result.toString();
   }
 
 }
