221ce0f9290f5e0d489f09c348aee3f774b931f4
Jan Dockx <jan_dockx@peopleware.be>
2008-06-20 22:06:51 +0000

    Cleaned.
    git-svn-id: file:///repositories/PPWCode/java/bean/trunk/src@1212 6057c1f7-a7c9-48c9-a9a6-1240b2ac66dc

--- main/java/org/ppwcode/i18n_I/ResourceBundleLoadStrategy.java
+++ main/java/org/ppwcode/i18n_I/ResourceBundleLoadStrategy.java
@@ -1,13 +1,31 @@
 /*<license>
-  Copyright 2004, PeopleWare n.v.
-  NO RIGHTS ARE GRANTED FOR THE USE OF THIS SOFTWARE, EXCEPT, IN WRITING,
-  TO SELECTED PARTIES.
+Copyright 2004 - $Date$ by PeopleWare n.v..
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 </license>*/
 
-package be.peopleware.i18n_I;
+package org.ppwcode.i18n_I;
 
+
+import static org.ppwcode.metainfo_I.License.Type.APACHE_V2;
 
 import java.util.ResourceBundle;
+
+import org.ppwcode.metainfo_I.Copyright;
+import org.ppwcode.metainfo_I.License;
+import org.ppwcode.metainfo_I.vcs.SvnInfo;
+import org.toryt.annotations_I.Expression;
+import org.toryt.annotations_I.MethodContract;
 
 
 /**
@@ -22,22 +40,11 @@ import java.util.ResourceBundle;
  * @author    Jan Dockx
  * @author    PeopleWare n.v.
  */
+@Copyright("2004 - $Date$, PeopleWare n.v.")
+@License(APACHE_V2)
+@SvnInfo(revision = "$Revision$",
+         date     = "$Date$")
 public interface ResourceBundleLoadStrategy {
-
-
-  /* <section name="Meta Information"> */
-  //------------------------------------------------------------------
-  /** {@value} */
-  String CVS_REVISION = "$Revision$"; //$NON-NLS-1$
-  /** {@value} */
-  String CVS_DATE = "$Date$"; //$NON-NLS-1$
-  /** {@value} */
-  String CVS_STATE = "$State$"; //$NON-NLS-1$
-  /** {@value} */
-  String CVS_TAG = "$Name$"; //$NON-NLS-1$
-
-  /* </section> */
-
 
   /**
    * Try to load the resource bundle with name <code>basename</code>,
@@ -45,13 +52,15 @@ public interface ResourceBundleLoadStrategy {
    * resource bundle can be found with this strategy, <code>null</code>
    * is returned.
    *
-   * @param     basename
-   *            The basename of the resource bundle that should be loaded.
-   * @result    (basename == null) ==> (result == null);
-   * @result    (basename.equals("")) ==> (result == null);
-   * @result    ; if there is no matching resource bundle,
-   *            <code>result == null</code>
+   * @param basename
+   *        The basename of the resource bundle that should be loaded.
    */
+  @MethodContract(
+    post = {
+      @Expression("_basename == null ? result == null"),
+      @Expression("_basename == EMPTY ? result == null"),
+    }
+  )
   ResourceBundle loadResourceBundle(final String basename);
 
 }
